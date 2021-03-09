2033669fc58e1a00dac69c13176d086b5fd1d002
David Sword <david@davidsword.ca>
2021-03-08 16:10:48 -0800

    add Jira to config

--- _config-sample.php
+++ _config-sample.php
@@ -8,3 +8,8 @@ define( 'HOME_ASSISTANT_HOST_URL', '' );
 
 define( 'HOME_ASSISTANT_BUSYLIGHT_ENTITY_FRIENDLY', '');
 define( 'HOME_ASSISTANT_BUSYLIGHT_ENTITY', '');
+
+define( 'JIRA_ASSIGNEE', '' );
+define( 'JIRA_USER', '' );
+define( 'JIRA_TOKEN', '' ); // https://id.atlassian.com/manage-profile/security/api-tokens revoked on 2021-02-09, not using it enough.
+define( 'JIRA_BOARD', 0 );

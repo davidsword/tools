1e01c12a1576477449a9c6944879d1370c3ecd03
David Sword <david@davidsword.ca>
2021-06-30 12:51:00 -0700

    improve readme

--- app-open-single-instance.sh
+++ app-open-single-instance.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# Script for shortcut to switch to active window if one exists, 
-# instead of opening a new window
+# Script for custom launcher shortcuts to switch to active window 
+# if one exists, instead of opening a new window
 #
-# Eg for terminal super+t custom shortcut: 
+# Eg. for terminal super+t custom shortcut: 
 # > /app-open-single-instance.sh gnome-terminal
 #
 # @credit: https://askubuntu.com/questions/82273/single-instance-of-the-gnome-terminal
+# @requires: wmctrl
 
 if ps ax | grep -v grep | grep $@ > /dev/null
 then
